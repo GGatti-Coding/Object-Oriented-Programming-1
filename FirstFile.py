a4d46878c4652c51ac8181ee2d8759d321038f48
GGatti-Coding <ggatti.guilherme@gmail.com>
2025-09-16 09:42:59 -0600

    First commit

---
+++ FirstFile.py
@@ -0,0 +1,16 @@
+# Guilherme Gatti.
+# This is for learning.
+# Not used for assignements or projects.
+
+#Variables
+im_hungry = "yes"
+pizzas = 3
+pizza_slices = 8
+delivery_time_mins = 30
+
+print("Am I hungry: " +im_hungry)
+print("Number of pizzas I'm eating: " +str(pizzas))
+print("Slices per pizza: " +str(pizza_slices))
+print("Time it will take to get here (in mins): " +str(delivery_time_mins))
+
+print("hi guys")
